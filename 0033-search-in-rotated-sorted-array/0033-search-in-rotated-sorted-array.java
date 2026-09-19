d59a2c7c2e053c2e1195068b354b3e456d92014b
Addalalakshmijanaki <addalalakshmijanaki2007@gmail.com>
2026-09-19 21:22:54 +0530

    Time: 0 ms (100%), Space: 44 MB (11.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,28 +1,28 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low =0;
-        int high=nums.length-1;
-        while(low<=high)
-        {
+       int low=0;
+       int high=nums.length-1;
+       while(low<=high)
+       {
             int mid=low+(high-low)/2;
             if(target==nums[mid])
             {
                 return mid;
             }
-            else if(nums[low]<=nums[mid])
+            if(nums[low]<=nums[mid])
             {
-                 if(target>=nums[low] && target<nums[mid])
-                 {
+                if(nums[low]<=target && target<nums[mid])
+                {
                     high=mid-1;
-                 }
-                 else
-                 {
-                   low=mid+1;
-                 }
+                }
+                else
+                {
+                    low=mid+1;
+                }
             }
             else
             {
-                if(target>nums[mid] && target<=nums[high])
+                if(nums[mid]<target && target<=nums[high])
                 {
                     low=mid+1;
                 }
@@ -31,9 +31,8 @@ class Solution {
                     high=mid-1;
                 }
             }
-        
-            
-        }
-        return -1;
+
+       }
+       return -1;
     }
 }
